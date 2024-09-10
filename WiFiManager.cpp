a9e380af4221df9bf7847f76737dd284cf465410
sshcrack <34072808+sshcrack@users.noreply.github.com>
2024-09-10 18:29:02 +0200

    don't add update routes if updating is disabled

--- WiFiManager.cpp
+++ WiFiManager.cpp
@@ -655,9 +655,11 @@ void WiFiManager::setupHTTPServer(){
   server->on(WM_G(R_status),     std::bind(&WiFiManager::handleWiFiStatus, this));
   server->onNotFound (std::bind(&WiFiManager::handleNotFound, this));
   
-  server->on(WM_G(R_update), std::bind(&WiFiManager::handleUpdate, this));
-  server->on(WM_G(R_updatedone), HTTP_POST, std::bind(&WiFiManager::handleUpdateDone, this), std::bind(&WiFiManager::handleUpdating, this));
-  
+  if(_showInfoUpdate) {
+    server->on(WM_G(R_update), std::bind(&WiFiManager::handleUpdate, this));
+    server->on(WM_G(R_updatedone), HTTP_POST, std::bind(&WiFiManager::handleUpdateDone, this), std::bind(&WiFiManager::handleUpdating, this));
+  }
+
   server->begin(); // Web server start
   #ifdef WM_DEBUG_LEVEL
   DEBUG_WM(WM_DEBUG_VERBOSE,F("HTTP server started"));
